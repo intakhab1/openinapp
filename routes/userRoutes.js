86261f403993ea799906b36e32b8e31ff1073b02
Mohd Intakhab Alam <amirint190@gmail.com>
2024-02-29 05:11:01 +0530

    Fix userAuth

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const userController = require('../controllers/userController');
+const { userMiddleware } = require("../middleware/auth")
 const router = express.Router();
 
-router.post('/create', userController.createUser);
-router.get('/getAll', userController.getAllUsers);
+router.post('/create', userMiddleware, userController.createUser);
+router.get('/getAll', userMiddleware, userController.getAllUsers);
 
 module.exports = router;
 
